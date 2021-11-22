006ae95fa8b4aa944a16b9db46c110d9a7795a6e
Leole11 <45719187+Leole11@users.noreply.github.com>
2021-11-22 13:13:50 -0400

    Added comments expanded functions
    - Added comments for clarity
    - Expanded the function "levelMasteryWithPool" since it also updates the UI, reverting the changed from this mod. Now the changes are permantent as long as you're loading the mod

--- BAL.user.js
+++ BAL.user.js
@@ -13,32 +13,49 @@
     'use strict';
 
     function updateAstrology() {
+        // if relevant element exists
         if (document.getElementById("astrology-bonus-element") != null ) {
-            var normalCol = document.createElement("div")
-            normalCol.setAttribute("class", "col-6 p-2")
-            var specialCol = document.createElement("div")
-            specialCol.setAttribute("class", "col-6 p-2")
 
-            var element = document.getElementById("astrology-bonus-element")
-            var container = element.children[0].children[0]
+            // create my new column elements
+            var normalCol = document.createElement("div");
+            normalCol.setAttribute("class", "col-6 p-2");
+            var specialCol = document.createElement("div");
+            specialCol.setAttribute("class", "col-6 p-2");
 
+            // container is the div containing all the rows that will be moved
+            var element = document.getElementById("astrology-bonus-element");
+            var container = element.children[0].children[0];
+
+            // flip flop
             var isItCommon = true;
 
+            // clone each row to their respective column, then remove them
             for(let i=1; i != container.childElementCount ; ){
-                isItCommon ? normalCol.appendChild(container.children[i].cloneNode(true)) : specialCol.appendChild(container.children[i].cloneNode(true))
-                container.removeChild(container.children[i])
-                isItCommon = !isItCommon
+                isItCommon ? normalCol.appendChild(container.children[i].cloneNode(true)) : specialCol.appendChild(container.children[i].cloneNode(true));
+                container.removeChild(container.children[i]);
+                isItCommon = !isItCommon;
             }
 
-            container.appendChild(normalCol)
-            container.appendChild(specialCol)
+            // append new columns
+            container.appendChild(normalCol);
+            container.appendChild(specialCol);
         }
     }
 
-    var old = createAstrologySelectedUI
+    var oldAstrology = createAstrologySelectedUI;
+    var oldMastery = levelUpMasteryWithPool;
 
+    // expand the definition of the function that updates the UI with new columns
     createAstrologySelectedUI = function(args) {
-        old.call(this, args)
-        updateAstrology()
+        oldAstrology.call(this, args);
+        updateAstrology();
     }
+
+    // spending mastery pool also updates the UI, so the columns need to be created, again
+    levelUpMasteryWithPool = function(skill, mastery, confirmation) {
+    oldMastery.call(this, skill, mastery, confirmation);
+    if (skill == 22 && !confirmation) {
+        updateAstrology();
+    }
+}
 })();
